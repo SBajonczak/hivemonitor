054bdbb736f3969d35fe3b03c27e4808760b60c5
Sascha Peter Bajonczak <Sascha.Bajonczak@realcore.de>
2020-03-05 12:03:13 +0100

    Added firmware flags

--- src/node-wifi-mqtt-homie.cpp
+++ src/node-wifi-mqtt-homie.cpp
@@ -6,8 +6,12 @@
   #include <RunningMedian.h>
   #include <Ticker.h>
 
-  #define FW_NAME "node-wifi-mqtt-homie-battery";
+  #define FW_NAME "hive-battery"
   #define FW_VERSION "0.10.0"
+
+  const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+  const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+
   const int DEFAULT_SLEEP_TIME = 20;
   #define USE_SCALE true
 
