42462fe922f35ae0d2c0a8b4b5eaf1a3ff5f7b63
Sascha Peter Bajonczak <Sascha.Bajonczak@realcore.de>
2021-08-06 10:46:24 +0200

    Refactored Settings class Storign data to spiff and removed homie reference

--- src/ConfigurationManager.cpp
+++ src/ConfigurationManager.cpp
@@ -1,4 +1,15 @@
 #include <ConfigurationManager.h>
+#include <EEPROM.h> // Reference for Memmory handling
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <FS.h> // Include the SPIFFS library
+#define CONFIG_FILE "/config.json"
+
+// Avoiding compile issues
+ConfigurationManager *ConfigurationManager::instance = nullptr;
+
+#define JSON_DOCSIZE 1024
+
 const int DEFAULT_SLEEP_TIME = 20;
 /* 
     Use sketch BeeScale-Calibration.ino to determine these calibration values.
@@ -8,7 +19,6 @@ const long DEFAULT_WEIGHT_OFFSET = 666;                   // Load cell zero offs
 const float DEFAULT_KILOGRAM_DIVIDER = 21.59;             // Load cell value per kilogram.
 const float DEFAULT_CALIBRATION_TEMPERATURE = 23.44;      // Temperature at which the scale has been calibrated for Temperature compensation
 const float DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE = 0.0; // Calibration factor in gram per degree
-
 /*
   * *Some* ESPs tend to have a too low reading on ESP.getVcc()
   * To get the maximum battery runtime you should check what ADC is reading and compare
@@ -17,51 +27,151 @@ const float DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE = 0.0; // Calibration factor 
   */
 const float DEFAULT_VCC_ADJUST = 0.23;
 
-HomieSetting<long> sleepTimeSetting("sleepTime", "SleepTime in seconds (max. 3600s!), default is 60s");
-HomieSetting<long> weightOffsetSetting("weightOffset", "Offset value to zero.");
-HomieSetting<double> kilogramDividerSetting("kilogramDivider", "Scale value per kilogram.");
-HomieSetting<double> calibrationTemperatureSetting("calibrationTemperature", "Outside Temperature at which the scale has been calibrated");
-HomieSetting<double> calibrationFactorSetting("calibrationFactor", "Calibration Factor in gram per degree. 0.0 to disable adjustment");
-HomieSetting<double> vccAdjustSetting("vccAdjust", "Calibration value for input voltage. See sketch for details.");
-
 void ConfigurationManager::setup()
 {
+  this->SleepTime = DEFAULT_SLEEP_TIME;
+  this->WeightOffset = DEFAULT_WEIGHT_OFFSET;
+  this->KilogramDivider = DEFAULT_KILOGRAM_DIVIDER;
+  this->VccAdjustSetting = DEFAULT_VCC_ADJUST;
+  this->CalibrationFactorSetting = DEFAULT_CALIBRATION_TEMPERATURE;
+  this->CalibrationFactorSetting = DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE;
+}
 
-  sleepTimeSetting.setDefaultValue(DEFAULT_SLEEP_TIME);
-  /*
-      * Can't set more than one value to default, see issue #323
-      * https://github.com/marvinroger/homie-esp8266/issues/323
-      */
-  weightOffsetSetting.setDefaultValue(DEFAULT_WEIGHT_OFFSET);
-  kilogramDividerSetting.setDefaultValue(DEFAULT_KILOGRAM_DIVIDER);
-  vccAdjustSetting.setDefaultValue(DEFAULT_VCC_ADJUST);
-  calibrationTemperatureSetting.setDefaultValue(DEFAULT_CALIBRATION_TEMPERATURE);
-  calibrationFactorSetting.setDefaultValue(DEFAULT_CALIBRATION_FACTOR_GRAM_DEGREE);
+ConfigurationManager::ConfigurationManager()
+{
+  //this->ReadSettings();
 }
 
 long ConfigurationManager::GetSleepTime()
 {
-  return sleepTimeSetting.get();
+  return this->SleepTime;
 }
 long ConfigurationManager::GetWeightOffset()
 {
 
-  return weightOffsetSetting.get();
+  return this->WeightOffset;
 }
 double ConfigurationManager::GetKilogramDivider()
 {
-  return kilogramDividerSetting.get();
+  return this->KilogramDivider;
 }
 double ConfigurationManager::GetCalibrationTemperatureSetting()
 {
-  return calibrationTemperatureSetting.get();
+  return this->CalibrationTemperatureSetting;
 }
 double ConfigurationManager::GetCalibrationFactorSetting()
 {
-  return calibrationFactorSetting.get();
+  return this->CalibrationFactorSetting;
 }
 double ConfigurationManager::GetVccAdjustSetting()
 {
 
-  return vccAdjustSetting.get();
+  return this->VccAdjustSetting;
+}
+
+String ConfigurationManager::GetWifiSsid() { return this->WifiSsid; }
+String ConfigurationManager::GetWifiPassword() { return this->WifiPassword; }
+String ConfigurationManager::GetMqttServer() { return this->MqttServer; }
+int ConfigurationManager::GetMqttPort() { return this->MqttPort; }
+String ConfigurationManager::GetMqttUser() { return this->MqttUser; }
+String ConfigurationManager::GetMqttPassword() { return this->MqttPassword; }
+
+String ConfigurationManager::GetJson()
+{
+  String values;
+  DynamicJsonDocument doc = this->GetJsonDocument();
+  serializeJson(doc, values);
+  return values;
+}
+
+DynamicJsonDocument ConfigurationManager::GetJsonDocument()
+{
+  DynamicJsonDocument doc(JSON_DOCSIZE);
+  doc["system"]["sleeptime"] = this->GetSleepTime();
+  doc["system"]["vccadjustsetting"] = this->GetVccAdjustSetting();
+  doc["scale"]["weightoffset"] = this->GetWeightOffset();
+  doc["scale"]["kilogramdivider"] = this->GetKilogramDivider();
+  doc["scale"]["calibrationtemperaturesetting"] = this->GetCalibrationTemperatureSetting();
+  doc["scale"]["calibrationfactorsetting"] = this->GetCalibrationFactorSetting();
+  doc["wireles"]["ssid"] = this->GetWifiSsid();
+  doc["wireles"]["password"] = this->GetWifiPassword();
+  doc["mqtt"]["server"] = this->GetMqttServer();
+  doc["mqtt"]["port"] = this->GetMqttPort();
+  doc["mqtt"]["user"] = this->GetMqttUser();
+  doc["mqtt"]["password"] = this->GetMqttPassword();
+  return doc;
+}
+
+void ConfigurationManager::ApplyJsonInput(String json)
+{
+  DynamicJsonDocument jData(2048);
+  Serial.println(json);
+  deserializeJson(jData, json);
+
+  this->WeightOffset = jData["scale"]["weightoffset"];
+  this->KilogramDivider = jData["scale"]["kilogramdivider"];
+  this->CalibrationTemperatureSetting = jData["scale"]["calibrationtemperaturesetting"];
+  this->CalibrationFactorSetting = jData["scale"]["calibrationfactorsetting"];
+
+  this->SleepTime = jData["system"]["sleeptime"];
+  this->VccAdjustSetting = jData["system"]["vccadjustsetting"];
+
+  this->MqttServer = jData["mqtt"]["server"].as<String>();
+  this->MqttUser = jData["mqtt"]["user"].as<String>();
+  this->MqttPassword = jData["mqtt"]["password"].as<String>();
+  this->MqttPort = jData["mqtt"]["port"];
+
+  this->WifiSsid = jData["wireles"]["ssid"].as<String>();
+  this->WifiPassword = jData["wireles"]["password"].as<String>();
+
+
+  if (jData.isNull())
+  {
+
+  }
+
+  // else
+  // {
+  //   Serial.println("is null");
+  // }
+  // this->WifiPassword = jData["wireles"]["password"].as<String>();
+  // this->WifiSsid = jData["wireles"]["ssid"].as<String>();
+}
+
+void ConfigurationManager::StoreSettings()
+{
+  String json = this->GetJson();
+  if (SPIFFS.begin())
+  {
+    SPIFFS.remove(CONFIG_FILE);
+    File configFile = SPIFFS.open(CONFIG_FILE, "w");
+    if (configFile)
+    {
+      Serial.println("Saving content: ");
+      Serial.println(json);
+      configFile.println(json);
+      configFile.close();
+    }
+  }
+}
+
+void ConfigurationManager::ReadSettings()
+{
+
+  if (SPIFFS.begin())
+  {
+    if (SPIFFS.exists(CONFIG_FILE))
+    {
+
+      File configFile = SPIFFS.open(CONFIG_FILE, "r");
+      if (configFile)
+      {
+        Serial.print("Read content:");
+        String s = configFile.readString();
+        this->ApplyJsonInput(s);
+        Serial.println("Done!✔");
+        return;
+      }
+    }
+  }
 }
--- src/ConfigurationManager.h
+++ src/ConfigurationManager.h
@@ -10,16 +10,56 @@ class ConfigurationManager
 {
 
 public:
+  ConfigurationManager();
   void setup();
-
   long GetSleepTime();
   long GetWeightOffset();
   double GetKilogramDivider();
   double GetCalibrationTemperatureSetting();
   double GetCalibrationFactorSetting();
   double GetVccAdjustSetting();
+  void ApplyJsonInput(String json);
+
+  String GetWifiSsid();
+  String GetWifiPassword();
+  String GetMqttServer();
+  int GetMqttPort();
+  String GetMqttUser();
+  String GetMqttPassword();
+
+  String GetJson();
+  void StoreSettings();
+  void ReadSettings();
+
+  // Get an Singelton instance
+  static ConfigurationManager *getInstance()
+  {
+    if (instance == nullptr)
+    {
+      instance = new ConfigurationManager();
+    }
+    return instance;
+  }
+
 private:
+  static ConfigurationManager *instance;
+  float VccAdjustSetting;
   int SleepTime;
+  String WifiSsid;
+  String WifiPassword;
+
+  String MqttServer;
+  String MqttUser;
+  int MqttPort;
+  String MqttPassword;
+
+  // weight
+  float WeightOffset;
+  float KilogramDivider;
+  float CalibrationTemperatureSetting;
+  float CalibrationFactorSetting;
   byte CurrentState;
+
+  DynamicJsonDocument GetJsonDocument();
 };
 #endif
